0d46d7e0c96395d9185c134ae2966a63a63b3826
lenovo <lenovo@DESKTOP-G3RV5V6>
2024-12-28 13:20:35 +0530

    Class members and OOPS Overview

---
+++ ConsoleApp_Batch_11_30_to_1/Methods/MethodDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Batch_11_30_to_1.Methods
+{
+    public class SmartCalcultor
+    {
+      //access-specifiers  return-type  method-name([parameter list])
+      //{ method block}
+
+        //instance Method
+        public int Sum(int x, int y)
+        {
+            //int sum = x + y;
+            //return sum;
+
+            return x + y;
+        }
+
+        public static void testStaticMethod()
+        {
+            Console.WriteLine("This is a static method.");
+        }
+    }
+    internal class MethodDemo
+    {
+        static void Main(string[] args)
+        {
+            SmartCalcultor smartCalcultor = new SmartCalcultor();
+
+             int result =  smartCalcultor.Sum(10, 20);
+            Console.WriteLine("Sum : " + result );
+
+
+           SmartCalcultor.testStaticMethod();
+        }
+    }
+}
---
+++ ConsoleApp_Batch_11_30_to_1/Methods/StaticAndInstanceMembers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Batch_11_30_to_1.Methods
+{
+    public class Student
+    {
+        public int RollNo;
+        public string Name;
+        public static int Count = 0;
+        public Student()
+        {
+            Count++;
+        }
+    }
+    internal class StaticAndInstanceMembers
+    {
+        static void Main(string[] args)
+        {
+
+            Student s1 = new Student()
+            {
+                RollNo = 1,
+                Name = "John"
+            };
+
+
+
+            Student s2 = new Student()
+            {
+                RollNo = 2,
+                Name = "ABC"
+            };
+
+
+
+            Student s3 = new Student()
+            {
+                RollNo = 3,
+                Name = "XYZ"
+            };
+
+
+
+            //Console.WriteLine($"Roll No : {s3.RollNo}  Name : {s3.Name}");
+
+            Console.WriteLine("Total Students : " + Student.Count);
+
+            //Roll No. : 1 Name '~John'
+            //Console.WriteLine("Roll No. : " + s3.RollNo + " Name : '~" + s3.Name + "'");
+            //Console.WriteLine("Roll No. : {0}  Name : '~{1}'",s3.RollNo,s3.Name);
+
+            //Console.WriteLine($"Roll No. {s3.RollNo} Name : '~{s3.Name}'");
+        }
+    }
+}
