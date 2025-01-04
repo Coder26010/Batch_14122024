8e05334c6baa47594428ea78e6670d79b87daf22
lenovo <lenovo@DESKTOP-G3RV5V6>
2025-01-04 13:46:44 +0530

    Added a module for exception handling.

---
+++ ConsoleApp_Batch_11_30_to_1/ExceptionHandling/ExceptionDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Batch_11_30_to_1.ExceptionHandling
+{
+    internal class ExceptionDemo
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
