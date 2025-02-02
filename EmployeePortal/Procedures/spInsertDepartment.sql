42bebc20e4588e900bff272dcb8de71b9ad40668
lenovo <lenovo@DESKTOP-G3RV5V6>
2025-02-02 14:05:35 +0530

    DB Script

--- EmployeePortal/Procedures/spInsertDepartment.sql
+++ EmployeePortal/Procedures/spInsertDepartment.sql
@@ -1,3 +1,26 @@
+USE [ADOEXAMPLEDB]
+GO
+CREATE TABLE [dbo].[TDEPARTMENT](
+	[SystemNumber] [int] IDENTITY(1,1) NOT NULL,
+	[DCode] [varchar](6) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Description] [varchar](200) NULL,
+	[RecordCreationDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[SystemNumber] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[DCode] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[TDEPARTMENT] ADD  DEFAULT (getdate()) FOR [RecordCreationDate]
+-----------------------------------------------------------------------------------------
+
+
 Create Proc spInsertDepartment
 @Dcode varchar(6),
 @Name varchar(50),
@@ -15,3 +38,22 @@ BEGIN
    select 'exists' as StatusCode
    return
 END;
+--------------------------------------------------------------------------------------------
+
+
+Create Procedure spGetAllDepartments
+AS
+BEGIN
+select SystemNumber,DCode,Name,Description,RecordCreationDate from TDEPARTMENT
+END;
+
+------------------------------------------------------------------------------------------
+Create Procedure spDeleteDepartment 10
+@SystemNumber int
+AS
+BEGIN
+Delete from TDEPARTMENT where SystemNumber = @SystemNumber
+END
+
+
+
